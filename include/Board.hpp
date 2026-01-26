bd22695b6e10b978e64d76b5969c9f6cb40768f7
Damien Bureau <damien.bureau14@gmail.com>
2026-01-26 17:42:41 +0100

    [Dev][Board] Add getter for frames + struct for board walls and nodes

--- include/Board.hpp
+++ include/Board.hpp
@@ -4,13 +4,22 @@
 
 #include "Frame.hpp"
 
+struct BoardStyle_t
+{
+    std::string horizontal_wall = "-----";
+    std::string vertical_wall = "|";
+    std::string wall_node = "+";
+};
+
 class Board
 {
 private:
     std::array<std::array<Frame, 3>, 3> frames; // X,Y
+    BoardStyle_t boardStyle;
 
 public:
     Board();
     ~Board();
+    std::array<std::array<Frame, 3>, 3> &getFrames();
     std::string toString() const;
 };
